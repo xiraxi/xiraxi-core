658caf8153f7b60cc2eb19459b68992540562653
Angel <wage@foton.es>
2010-08-20 19:53:07 +0100

    Step definition to check actual route action

--- features/step_definitions/pages.rb
+++ features/step_definitions/pages.rb
@@ -3,3 +3,7 @@ Then /^the flash box contains "([^"]*)"$/ do |content|
   page.should have_css(".flash-box")
   find(:css, ".flash-box").text.should include(content)
 end
+
+Then /^the current page is a "([^"]*)" action$/ do |action_name|
+  Rails.application.routes.recognize_path(current_path, Caulfield.env)[:action].should eql(action_name)
+end
