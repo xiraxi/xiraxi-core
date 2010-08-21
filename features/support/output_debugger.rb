ec6802d7b12b6492e09e61436650b6d951bd6364
Ayose <ayose@foton.es>
2010-08-21 17:12:40 +0100

    Support for ENV["XT"] for test
    Possible values:
      f    Save it on /tmp/cucumber_output.XXX.html
      s    Dump to stderr
    
    Usage:
    
      XT=s cucumber ..

---
+++ features/support/output_debugger.rb
@@ -0,0 +1,22 @@
+
+# Using XT (Xiraxi Tests) we can debug the output generated
+# Possible values
+#   f    Save it on /tmp/cucumber_output.XXX.html
+#   s    Dump to stderr
+After do
+  case ENV["XT"]
+  when "s"
+    STDERR.puts page.body
+  when "f"
+    n = 0
+    while n < 1000
+      n += 1
+      path = Pathname.new("/tmp/cucumber_output.%03d.html" % n)
+      unless path.exist?
+        path.open("w") {|f| f.write page.body }
+        break
+      end
+      STDERR.puts "Output written to #{path}"
+    end
+  end
+end
