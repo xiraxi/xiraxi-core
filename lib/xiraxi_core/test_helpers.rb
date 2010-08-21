8026343a4f917e2865ef266a0413d65076c5ea45
Ayose <ayose@foton.es>
2010-08-21 15:47:37 +0100

    Ignore others env.rb when initialize test environment

--- lib/xiraxi_core/test_helpers.rb
+++ lib/xiraxi_core/test_helpers.rb
@@ -9,7 +9,11 @@ module XiraxiCore::TestHelpers
             features_dir = engine.root.join("features")
             if features_dir.directory?
                 %w(support step_definitions factories).each do |base|
-                    Dir["#{features_dir}/#{base}/*.rb"].each {|filename| require filename }
+                    Dir["#{features_dir}/#{base}/*.rb"].each do |filename|
+                      next if filename =~ /features\/support\/env\.rb$/
+
+                      require filename
+                    end
                 end
             end
         end
