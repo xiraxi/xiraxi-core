f86bcf017a7819d5da136685aa5f578bc187e7dd
Ayose <ayose@foton.es>
2010-08-21 02:30:58 +0100

    Fix in the «then an email was sent with ...» step

--- features/step_definitions/emails.rb
+++ features/step_definitions/emails.rb
@@ -1,6 +1,6 @@
 
 Then /^an email was sent with (.*)$/ do |fields|
-  email = ActionMailer::Base.deliveries.first
+  email = ActionMailer::Base.deliveries.last
   fields.scan(/(\w+):\s*"([^"]*)"/).each do |field, value|
     email.send(field).to_s.should eql(value)
   end
