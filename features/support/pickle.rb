f403406fe208481deb74ae4e3ba0dac393669731
Ayose <ayose@foton.es>
2010-08-21 02:29:27 +0100

    Redefined Pickle::Session::Parser#parse_hash
    With this modification we can use timestamp expressions in table, like
    
        And the following things exists:
          | name  | something_at   |
          | Foo   | 1 day ago      |
          | Bar   | 10 minutes ago |

--- features/support/pickle.rb
+++ features/support/pickle.rb
@@ -8,3 +8,21 @@ Pickle.configure do |config|
    #config.map 'I', 'myself', 'me', 'my', :to => 'user: "me"'
 end
 
+
+# Extensions to Pickle
+module Pickle::Session::Parser
+
+  # Redefined to support expressions in tables
+  def parse_hash(hash)
+    hash.inject({}) do |parsed, (key, val)|
+      if session && val =~ /^#{capture_model}$/
+        parsed.merge!(key => session.model($1))
+      elsif val =~ /^\s*(\d+)\s*(minutes?|days?|seconds?)\s*ago\s*$/
+        parsed.merge!(key => $1.to_i.send($2).ago)
+          parsed
+      else
+        parsed.merge!(key => val)
+      end
+    end
+  end
+end
