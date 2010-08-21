3d1e1cce427e9047bbdba6685e914249fe26672f
Ayose <ayose@foton.es>
2010-08-21 02:31:31 +0100

    Implemented «then the "..." box has these boxes in the same order» step

--- features/step_definitions/dom_tree.rb
+++ features/step_definitions/dom_tree.rb
@@ -38,3 +38,32 @@ end
 Then /^the page contains "([^"]*)"$/ do |content|
   page.should have_content(content)
 end
+
+Then /^the "([^"]*)" box has these boxes in the same order:$/ do |box, table|
+  rows = table.raw.dup
+  row_waiting = true
+  field = content = nil
+
+  find_by_query(box).node.search("*").each do |node|
+    next unless node.element?
+
+    if row_waiting
+      field, content = rows.shift
+      break if field.nil?
+
+      field = field.downcase.tr(' ', '_')
+      row_waiting = false
+    end
+
+    if  node["id"].to_s.downcase == field ||                         # ID
+        " #{node["class"].to_s.downcase} ".include?(" #{field} ") || # CSS class
+        node["data-locator"].to_s.downcase == field
+
+      node.inner_text.to_s.should include(content)
+      row_waiting = true
+
+    end
+  end
+
+  rows.should eql([])
+end
