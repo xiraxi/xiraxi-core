c5c15d6589cf48b37c9b946505424ff237941bdc
Angel <wage@foton.es>
2010-08-22 21:15:18 +0100

    Tag cloud

--- lib/xiraxi_core/page_helpers.rb
+++ lib/xiraxi_core/page_helpers.rb
@@ -18,6 +18,20 @@ module XiraxiCore::PageHelpers
     link_to(t("actions.delete"), url, :class => "button", :method => :delete, :confirm => t("confirm.delete"))
   end
 
+  def tag_cloud_generator(model)
+    url_proc = params.kind_of?(Hash) ? (proc {|tag| tag ? params.merge(:tag => tag.name) : "#" }) : params
+    @tag_selected ||= params[:tag]
+    result = '<div class="tag-cloud" class="box">'
+    result << "<h2>#{t("tag_cloud.title")}</h2>"
+    result << "<div class='content'>"
+    tag_cloud(model.tag_counts_on(:tags), %w(tag1 tag2 tag3 tag4 tag5)) do |tag, css_class|
+      selected = @tag_selected == tag.name
+      css_class = "#{css_class} selected" if selected
+      result << link_to(tag.name,url_proc.call(selected ? nil : tag), :class => css_class) 
+    end
+    result << '</div></div>'
+    return result.html_safe
+  end
 end
 
 require 'action_view/base'
