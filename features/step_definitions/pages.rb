efc3c3be3adc9edfebc02dbb0763cb7a52ff104e
Angel <wage@foton.es>
2010-08-22 18:58:39 +0100

    Count boxes step definition

--- features/step_definitions/pages.rb
+++ features/step_definitions/pages.rb
@@ -7,3 +7,7 @@ end
 Then /^the current page is a "([^"]*)" action$/ do |action_name|
   Rails.application.routes.recognize_path(current_path, Caulfield.env)[:action].should eql(action_name)
 end
+
+Then /^I see (\d+) "([^"]+)" boxes/ do |count, css_class|
+  all(:css, ".#{css_class}").size.should eql(count.to_i)
+end
