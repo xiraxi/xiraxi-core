ec29977ad7a1f41f1f19be194e76382d5a6683e1
Ayose <ayose@foton.es>
2010-11-10 17:34:17 +0000

    Some methods for testing with Capybara

--- lib/xiraxi_core/test_helpers.rb
+++ lib/xiraxi_core/test_helpers.rb
@@ -12,10 +12,28 @@ module ExtraCapybaraHelpers
     assert page.has_xpath?(xpath), message || "page sould have #{xpath}"
   end
 
+  def assert_not_xpath(xpath, message = nil)
+    assert !page.has_xpath?(xpath), message || "page sould hot have #{xpath}"
+  end
+
   def assert_flash_box(content, flash_type = nil)
     box = find(:css, flash_type ? "div.flash-box.#{flash_type}" : "div.flash-box")
     assert_not_nil box
     assert_match content, box.native.inner_text
+  end
+
+  def cookies
+    # So ugly...
+    Capybara.current_session.driver.current_session.instance_variable_get(:@rack_mock_session).cookie_jar
+  end
+
+  def clear_session_cookies
+    cookies = self.cookies
+    cookies.send(:hash_for).each_pair do |name, cookie|
+      if cookie.expires.nil?
+        cookies[name] = nil
+      end
+    end
   end
 end
 
